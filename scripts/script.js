dd151a431622d439ef1a28447ef13694210acdaf
goncalves-ac <53871994+goncalves-ac@users.noreply.github.com>
2020-06-07 16:52:34 -0300

    Mais Funções

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,58 @@
+function quebraLinha(){
+  let textarea = document.getElementById("texto");
+  let edit = document.getElementById("change");
 
-let textarea = document.getElementById("texto");
-let edit = document.getElementById("change");
+  edit.addEventListener("click", () => {
+    let texto = textarea.value;
+    let resultado = "";
 
-edit.addEventListener("click", () => {
-  let texto = textarea.value;
-  let resultado = "";
+    for (i = 0; i < texto.length; i++) {
+      texto.charCodeAt(i) == 10 ?
+      resultado += " " :
+      resultado += texto[i];
+    }
+
+    console.log(resultado)
+    textarea.value = resultado;
+  })  
+}
+
+function fazerCopia() {
+  let range = document.createRange();
+  range.selectNode(document.getElementById("textoFinal"));
+  window.getSelection().removeAllRanges(); // limpa seleção atual
+  window.getSelection().addRange(range); // seleciona o texto
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();// limpa seleção
+}
+
+function limparTela(){
+  document.getElementById("texto").value='';
+  textoFinal.innerHTML = '';
+}
+
+function fazerSelecao(){
+  var novoTexto = [];
+
+  var textoPego = document.getElementById('texto').value;
+  var arrayTexto = textoPego.split(' ');
   
-  for (let i = 0; i < texto.length; i++) {
-    texto.charCodeAt(i) == 10 ?
-    resultado += " " :
-    resultado += texto[i];
+  if(document.getElementById('palavra').value == "") {
+    var palavraAselecionar = document.querySelector('.garantia:checked').value;
+  } else {
+    var palavraAselecionar = document.getElementById('palavra').value;
   }
   
-  console.log(resultado)
-  textarea.value = resultado;
-})
+
+  for (let i = 0; i < arrayTexto.length; i++) {
+    if (arrayTexto[i].toLowerCase() === palavraAselecionar.toLowerCase()){
+      var versaoNova = arrayTexto[i].toUpperCase().bold();
+      novoTexto.push(versaoNova);
+    } else {
+      novoTexto.push(arrayTexto[i]);
+   } 
+  }
+  
+  var novoTextoFinal = novoTexto.toString().replace(/,/gi, " ");
+  textoFinal.innerHTML = novoTextoFinal;
+}
